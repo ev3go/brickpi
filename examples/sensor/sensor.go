a09ac4689e22e6f65a62165d9c6dfd22c3de9f6b
kortschak <dan.kortschak@adelaide.edu.au>
2017-10-23 20:56:33 +1030

    example/sensor: update ev3dev API use

--- examples/sensor/sensor.go
+++ examples/sensor/sensor.go
@@ -36,20 +36,9 @@ func main() {
 	}
 	defer brickpi.Unregister(*port)
 
-	n, err := s.NumValues()
-	if err != nil {
-		log.Fatalf("failed to get number of values available: %v", err)
-	}
-
-	u, err := s.Units()
-	if err != nil {
-		log.Fatalf("failed to get units: %v", err)
-	}
-
-	d, err := s.Decimals()
-	if err != nil {
-		log.Fatalf("failed to get number of decimal places: %v", err)
-	}
+	n := s.NumValues()
+	u := s.Units()
+	d := s.Decimals()
 
 	addr, err := ev3dev.AddressOf(s)
 	if err != nil {
